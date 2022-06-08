b2eb8824aa43bc04939ceff0e7d298d9152edbad
Joau0824 <a20182162@pucp.edu.pe>
2022-06-08 01:56:48 -0500

    Distribuidora Get/post/put/delete

--- src/main/java/com/lab7/lab7_restfull/controller/DistribuidoraController.java
+++ src/main/java/com/lab7/lab7_restfull/controller/DistribuidoraController.java
@@ -4,13 +4,13 @@ package com.lab7.lab7_restfull.controller;
 import com.lab7.lab7_restfull.entity.Distribuidora;
 import com.lab7.lab7_restfull.repository.DistribuidorasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +21,50 @@ public class DistribuidoraController {
 
     @Autowired
     DistribuidorasRepository distribuidorasRepository;
+
+    @GetMapping(value="/api/distribuidora", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
+    public List<Distribuidora> listaDistribuidoras() {
+        List<Distribuidora> listaD = distribuidorasRepository.findAll();
+        return listaD;
+    }
+    @GetMapping(value="/distribuidora/{id}", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
+    public ResponseEntity<HashMap<String, Object>> obtenerDistribuidorasId(@PathVariable("id") String idstr){
+        HashMap<String, Object> responseJson = new HashMap<>();
+
+
+        try{
+            int id = Integer.parseInt(idstr);
+            Optional<Distribuidora> optionalDistribuidora = distribuidorasRepository.findById(id);
+            if(optionalDistribuidora.isPresent()){
+                responseJson.put("result","success");
+                responseJson.put("product",optionalDistribuidora.get());
+                return ResponseEntity.ok(responseJson);
+            }else{
+                responseJson.put("result","failure");
+                responseJson.put("msg","La distribuidora con ese Id no existe");
+                return ResponseEntity.ok(responseJson);
+            }
+        }catch(NumberFormatException e){
+            responseJson.put("result","failure");
+            responseJson.put("msg","El id debe ser un código numérico");
+            return ResponseEntity.badRequest().body(responseJson);
+        }
+    }
+
+    @PostMapping("/api/distribuidora")
+    public ResponseEntity<HashMap<String, Object>> crearDistribuidora(
+            @RequestBody Distribuidora distribuidora,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId){
+        HashMap<String, Object> responseJson = new HashMap<>();
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        distribuidorasRepository.save(distribuidora);
+        if(fetchId){
+            responseMap.put("id",distribuidora.getId());
+        }
+        responseMap.put("creado","ok");
+        return  ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
 
     @PutMapping(value = "/api/distribuidora")
     public ResponseEntity<HashMap<String,Object>> actualizarDistribuidora(@RequestBody Distribuidora distribuidora) {
@@ -61,60 +105,54 @@ public class DistribuidoraController {
     @PutMapping(value = "/api/distribuidora/parcial")
     public ResponseEntity<HashMap<String, Object>> actualizarDistribuidoraParcial(@RequestBody Distribuidora distribuidora) {
 
-    @GetMapping(value="/api/distribuidora", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
-    public List<Distribuidora> listaDistribuidoras(){
-        List<Distribuidora> listaD=distribuidorasRepository.findAll();
-        return listaD;
-    }
-
-    @GetMapping(value="/distribuidora/{id}", produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
-    public ResponseEntity<HashMap<String, Object>> obtenerDistribuidorasId(@PathVariable("id") String idstr){
-        HashMap<String, Object> responseJson = new HashMap<>();
-
-
-       try{
-           int id = Integer.parseInt(idstr);
-           Optional<Distribuidora> optionalDistribuidora = distribuidorasRepository.findById(id);
-           if(optionalDistribuidora.isPresent()){
-               responseJson.put("result","success");
-               responseJson.put("product",optionalDistribuidora.get());
-               return ResponseEntity.ok(responseJson);
-           }else{
-               responseJson.put("result","failure");
-               responseJson.put("msg","La distribuidora con ese Id no existe");
-               return ResponseEntity.ok(responseJson);
-           }
-       }catch(NumberFormatException e){
-           responseJson.put("result","failure");
-           responseJson.put("msg","El id debe ser un código numérico");
-           return ResponseEntity.badRequest().body(responseJson);
-       }
-    }
-
-    @PostMapping("/api/distribuidora")
-    public ResponseEntity<HashMap<String, Object>> crearDistribuidora(
-            @RequestBody Distribuidora distribuidora,
-            @RequestParam(value = "fetchId", required = false) boolean fetchId){
-        HashMap<String, Object> responseJson = new HashMap<>();
         HashMap<String, Object> responseMap = new HashMap<>();
 
-        distribuidorasRepository.save(distribuidora);
-        if(fetchId){
-            responseMap.put("id",distribuidora.getId());
+        if (distribuidora.getId() > 0) {
+            Optional<Distribuidora> opt = distribuidorasRepository.findById(distribuidora.getId());
+            if (opt.isPresent()) {
+                Distribuidora distribuidoraFromDb = opt.get();
+
+                if (distribuidora.getNombre() != null)
+                    distribuidoraFromDb.setNombre(distribuidora.getNombre());
+
+                if (distribuidora.getDescripcion() != null)
+                    distribuidoraFromDb.setDescripcion(distribuidora.getDescripcion());
+
+                if (distribuidora.getFundacion() != null)
+                    distribuidoraFromDb.setFundacion(distribuidora.getFundacion());
+
+                if (distribuidora.getWeb() != null)
+                    distribuidoraFromDb.setWeb(distribuidora.getWeb());
+
+                if (distribuidora.getIdsede() != null)
+                    distribuidoraFromDb.setIdsede(distribuidora.getIdsede());
+
+                distribuidorasRepository.save(distribuidoraFromDb);
+                responseMap.put("estado", "actualizado");
+                return ResponseEntity.ok(responseMap);
+            } else {
+                responseMap.put("msg", "La distribuidora a actualizar no existe");
+            }
+        } else {
+            responseMap.put("msg", "Debe enviar un ID");
         }
-        responseMap.put("creado","ok");
-        return  ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+        responseMap.put("estado", "error");
+        return ResponseEntity.badRequest().body(responseMap);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<HashMap<String, Object>> gestionarExcepcion(HttpServletRequest request){
-        HashMap<String, Object> responseJson = new HashMap<>();
-        if(request.getMethod().equals("POST")){
-            responseJson.put("estado", "error");
-            responseJson.put("msg","debes enviar el producto");
-        }
-        return  ResponseEntity.badRequest().body(responseJson);
-    }
+
+
+
+
+    //@ExceptionHandler(HttpMessageNotReadableException.class)
+    //public ResponseEntity<HashMap<String, Object>> gestionarExcepcion(HttpServletRequest request){
+    //    HashMap<String, Object> responseJson = new HashMap<>();
+    //    if(request.getMethod().equals("POST")){
+    //        responseJson.put("estado", "error");
+    //        responseJson.put("msg","debes enviar el producto");
+    //    }
+    //    return  ResponseEntity.badRequest().body(responseJson);
+    //}
 
     @DeleteMapping(value = "/api/distribuidora/{id}")
     public ResponseEntity<HashMap<String, Object>> borrarDistribuidora(@PathVariable("id") String idStr) {
