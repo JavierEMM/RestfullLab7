02360901afe72ae578a2ed689e37122ba34f96dc
Diego Vargas <a20191027@pucp.edu.pe>
2022-06-08 01:28:20 -0500

    juego apix2

--- src/main/java/com/lab7/lab7_restfull/controller/JuegosController.java
+++ src/main/java/com/lab7/lab7_restfull/controller/JuegosController.java
@@ -3,29 +3,36 @@ package com.lab7.lab7_restfull.controller;
 import com.lab7.lab7_restfull.entity.Juego;
 import com.lab7.lab7_restfull.repository.JuegosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-@Controller
+
+
+@RestController
 public class JuegosController {
 
     @Autowired
     JuegosRepository juegosRepository;
 
-    @ResponseBody
+
+
     @GetMapping(value = "/api/juegos")
     public List<Juego> listarJuegosApi(){
         List<Juego> lista = juegosRepository.findAll();
         return lista;
     }
 
-    @ResponseBody
+    @PostMapping(value = "/api/juegos")
+    public List<Juego> listarJuegosApiPOST(){
+        List<Juego> lista = juegosRepository.findAll();
+        return lista;
+    }
+
     @GetMapping(value = "/api/juegos/{id}")
     public ResponseEntity<HashMap<String,Object>> obtenerJuegoPorId(@PathVariable("id") String idStr){
         HashMap<String,Object> responseJson = new HashMap<>();
@@ -44,6 +51,20 @@ public class JuegosController {
         }
         responseJson.put("result","failure");
         return ResponseEntity.badRequest().body(responseJson);
+    }
+
+    @PostMapping(value = "/juegos")
+    public ResponseEntity<HashMap<String,Object>> guardarJuego(@RequestBody Juego juego,
+                                                                @RequestParam(value = "fetchId",required = false) boolean fetchId){
+
+        HashMap<String,Object> responseMap = new HashMap<>();
+        juegosRepository.save(juego);
+
+        if(fetchId){
+            responseMap.put("id",juego.getId());
+        }
+        responseMap.put("estado","creado");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
     }
 
 
